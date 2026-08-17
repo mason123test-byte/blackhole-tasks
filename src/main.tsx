85a103782e2adafa88088c0cae65cb94f522591b
mason123test-byte <mason123test@gmail.com>
2026-08-17 16:11:05 +0800

    fix: load expanded completion hit area

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { OrbApp } from "./app/OrbApp";
 import "./styles/global.css";
+import "./styles/native-hit-target.css";
 
 document.title = "黑洞任务";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><OrbApp/></React.StrictMode>);
