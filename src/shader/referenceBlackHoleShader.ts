afe81f62f0651b8c111a3da60f468dba017facd1
mason123test-byte <mason123test@gmail.com>
2026-08-18 20:00:16 +0800

    tune: restore broad physical Gargantua disk radius

--- src/shader/referenceBlackHoleShader.ts
+++ src/shader/referenceBlackHoleShader.ts
@@ -40,7 +40,7 @@ const float OBSERVER_THETA = 1.535;
 const float CAMERA_VERTICAL_FOV = 0.54105207;
 
 const float DISK_INNER = 9.26;
-const float DISK_OUTER = 17.30;
+const float DISK_OUTER = 18.70;
 const int MAX_DISK_CROSSINGS = 4;
 const float STAR_GAIN = 0.0;
 const float DILATION_MIN = 0.20;
